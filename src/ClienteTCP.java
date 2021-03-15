57e9906cfa79b969f2a8d92087396d140dd73c41
Nicolas Cardenas <n.cardenass@uniandes.edu.co>
2021-03-14 22:47:23 -0500

    Cambios

--- src/ClienteTCP.java
+++ src/ClienteTCP.java
@@ -10,80 +10,34 @@ import java.util.logging.Logger;
 
 public class ClienteTCP {
 
-	public final static int SOCKET_PORT = 13267;      // you may change this
-	public final static String SERVER = "127.0.0.1";  // localhost
-	
-	public final static String FILE_TO_RECEIVED = "./archivos";  // you may change this, I give a
-	// different name because i don't want to
-	// overwrite the one used by server...
-	
-	public final static String FILE_TO_RECEIVED2 = "./archivos";
-
-	public final static int FILE_SIZE = 6022386;
-	public final static int FILE_SIZE2 = 6022386;// file size temporary hard coded
-	// should bigger than the file to be downloaded
 
 
 	public static void main(String[] args) throws IOException {
 
-		int bytesRead;
-		int bytesRead2;
-		int current = 0;
-		int current2 = 0;
 		FileOutputStream fos = null;
-		FileOutputStream fos2 = null;
 		BufferedOutputStream bos = null;
-		BufferedOutputStream bos2 = null;
 		Socket sock = null;
+
 		try {
-			sock = new Socket(SERVER, SOCKET_PORT);
-			System.out.println("Connecting...");
-
-			// receive file
-			byte [] arreglo1  = new byte [FILE_SIZE];
-			byte [] arreglo2  = new byte [FILE_SIZE2];
-			
+			sock = new Socket("localhost", 9999);
+			System.out.println("Conectando");
+			byte [] arreglo  = new byte [6000000];
 			InputStream is = sock.getInputStream();
-			InputStream is2 = sock.getInputStream();
-			fos = new FileOutputStream(FILE_TO_RECEIVED);
-			fos2 = new FileOutputStream(FILE_TO_RECEIVED2);
+			fos = new FileOutputStream("C:\\Users\\nicoc\\Documents\\ArchivoDescargado.txt");
 			bos = new BufferedOutputStream(fos);
-			bos2 = new BufferedOutputStream(fos2);
-			bytesRead = is.read(arreglo1,0,arreglo1.length);
-			bytesRead2 = is2.read(arreglo2, 0, arreglo2.length);
-			current = bytesRead;
-			current2 = bytesRead2;
-
-			do {
-				bytesRead =
-						is.read(arreglo1, current, (arreglo1.length-current));
-				if(bytesRead >= 0) current += bytesRead;
-			} while(bytesRead > -1);
-
-			bos.write(arreglo1, 0 , current);
+			is.read(arreglo,0,arreglo.length);
+			System.out.println("Archivo recibido");
+			bos.write(arreglo, 0 , arreglo.length);
 			bos.flush();
-			System.out.println("File " + FILE_TO_RECEIVED
-					+ " downloaded (" + current + " bytes read)");
-			
-			do {
-				bytesRead2 =
-						is.read(arreglo2, current2, (arreglo2.length-current2));
-				if(bytesRead2 >= 0) current2 += bytesRead2;
-			} while(bytesRead2 > -1);
 
-			bos2.write(arreglo2, 0 , current2);
-			bos.flush();
-			System.out.println("File " + FILE_TO_RECEIVED2
-					+ " downloaded (" + current + " bytes read)");
-			
-			
-		}
-		finally {
 			if (fos != null) fos.close();
 			if (bos != null) bos.close();
-			if (bos2 != null) bos2.close();
 			if (sock != null) sock.close();
 		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+
 	}
 
 }
--- src/ServidorTCP.java
+++ src/ServidorTCP.java
@@ -1,95 +1,67 @@
 
 import java.io.BufferedInputStream;
-
 import java.io.File;
 import java.io.FileInputStream;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
-public class ServidorTCP extends Thread {
-	
-	
-    private final static String archivo1 = "";
-    private final static String archivo2 = "";
 
-	public static void main(String[] args) {
+public class ServidorTCP  {
+
+
+	public static void main(String[] args) throws Exception {
 		
+		System.out.println("El archivo 1 corresponde a 100MB");
+		System.out.println("El archivo 2 corresponde a 250MB");
+		System.out.println("¿Cual desea enviar?");
+
+		Scanner myInput = new Scanner( System.in );
+		int i = myInput.nextInt();
+		myInput.close();
+		if(i==1)
+		{
+			System.out.println("El archivo # " + i+" de 100MB será enviado");
+		}
+		else if (i==2)
+		{
+			System.out.println("El archivo #" + i +" de 250MB será enviado");
+		}
+		else
+		{
+			throw new Exception("El numero de archivo debe ser 1 o 2");
+		}
 		
 		ServerSocket servidor = null;
-		Socket sc = null;
-		
-
-		//puerto de nuestro servidor
-		final int PUERTO = 5000;
-		
-	
+		Socket sc = null;	
 		try {
-			//Creamos el socket del servidor
-			servidor = new ServerSocket(PUERTO);
+
+			servidor = new ServerSocket(9999);
 			System.out.println("Servidor iniciado");
 
-			//Siempre estara escuchando peticiones
 			while (true) {
-
-				//Espero a que un cliente se conecte
 				sc = servidor.accept();
 				System.out.println("Cliente conectado");
-				
-				File archivo1Envio = new File(archivo1);
-				File archivo2Envio = new File(archivo2);
-				
+				File archivo1Envio = new File("C:\\Users\\nicoc\\Documents\\test"+i+".txt");
 				byte[] arreglo = new byte[(int)archivo1Envio.length()];
-				byte[] arreglo2 = new byte[(int)archivo2Envio.length()];
-				
 				FileInputStream fis= new FileInputStream(archivo1Envio);
 				BufferedInputStream bis = new BufferedInputStream(fis);
-				
-				FileInputStream fis2= new FileInputStream(archivo2Envio);
-				BufferedInputStream bis2 = new BufferedInputStream(fis2);
-				
-				
-				bis.read(arreglo, 0, arreglo.length);
-				bis2.read(arreglo2, 0, arreglo2.length);
-				
 				OutputStream os = sc.getOutputStream();
-				System.out.println("Sending " + archivo1 + "(" + arreglo.length + " bytes)");
-				System.out.println("Sending " + archivo2 + "(" + arreglo2.length + " bytes)");
+				bis.read(arreglo, 0, arreglo.length);
 				os.write(arreglo, 0, arreglo.length);
-				os.write(arreglo2, 0, arreglo2.length);
-				
+
+
 				os.flush();
 				sc.close();	
 
 			}
 
-		} catch (IOException ex) {
-			Logger.getLogger(ServidorTCP.class.getName()).log(Level.SEVERE, null, ex);
+		} 
+		catch (IOException ex) {
 		}
 
-	}
-	
-	
-	public void start()
-	{
-		for(int i=1; i<25;i++)
-		{
-			ServidorTCP s = new ServidorTCP();
-			Thread t = new Thread(s);
-			t.start();
-	
-		}
-	}
-	
-	@Override
-    public void run() {
-
-    }
-
-	
+	}	
 
 }
